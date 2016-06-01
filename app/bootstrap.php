6308789d3fad5048c7051dc1cdf95c63c90ea055
Petr Horacek <petr.horacek@wunderman.cz>
2016-06-01 15:44:24 +0200

    Omezeni dev prostredi pouze na lokalni sit Wundermanu a vagrant stroj

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode(TRUE); // enable for your remote IP
+$configurator->setDebugMode(['80.95.101.194', '192.168.50.1']); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
